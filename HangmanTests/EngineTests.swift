392a097586a458dbcc2ea9c47159622e9c0c311a
Pepijn Looije <p@epij.nl>
2015-12-11 22:50:38 +0100

    Cleanup

--- HangmanTests/EngineTests.swift
+++ HangmanTests/EngineTests.swift
@@ -48,11 +48,4 @@ class EngineTests: XCTestCase {
         XCTAssertFalse(engine.wonGame)
         XCTAssertFalse(engine.guessLetter("d"))
     }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
-    }
 }
